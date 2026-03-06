282b66c44db65c9fab44cc1aa851c00d06f40b44
jeironEC <jeironespinal0306@gmail.com>
2026-03-06 16:33:57 +0100

    NI-45: creación de modelo Inventario

--- backend/nexus_inventory_backend/db/admin.py
+++ backend/nexus_inventory_backend/db/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Role, User, Category, Product
+from .models import Role, User, Category, Product, Inventory
 
 admin.site.register(Role)
 admin.site.register(User)
 admin.site.register(Category)
 admin.site.register(Product)
+admin.site.register(Inventory)

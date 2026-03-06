58ad1f8824c4235d7be32b3c8d745134bc37f13d
Jeiron Espinal <jeironespinal0306@gmail.com>
2026-03-06 17:36:46 +0100

    NI-46: creación de modelos Cliente y Promocion (#27)

--- backend/nexus_inventory_backend/db/admin.py
+++ backend/nexus_inventory_backend/db/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
-from .models import Role, User, Category, Product, Inventory
+from .models import (
+    Role,
+    User,
+    Category,
+    Product,
+    Inventory,
+    Customer,
+    Promotion,
+    CustomerPromotion,
+)
 
 admin.site.register(Role)
 admin.site.register(User)
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Inventory)
+admin.site.register(Customer)
+admin.site.register(Promotion)
+admin.site.register(CustomerPromotion)

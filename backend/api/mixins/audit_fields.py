204d85125f90e126b13d1b8f256a68167b7067dc
jeironEC <jeironespinal0306@gmail.com>
2026-03-21 16:53:45 +0100

    NI-70: implementación de auditoría de usuario en operaciones CRUD

--- backend/api/mixins/audit_fields.py
+++ backend/api/mixins/audit_fields.py
@@ -4,6 +4,9 @@ from rest_framework import serializers
 # Serializers
 from api.serializers.user_read import UserReadSerializer
 
+# Django
+from django.utils import timezone
+
 
 class AuditFieldsMixin(serializers.ModelSerializer):
     """
@@ -13,3 +16,20 @@ class AuditFieldsMixin(serializers.ModelSerializer):
     created_by = UserReadSerializer(read_only=True)
     updated_by = UserReadSerializer(read_only=True)
     deleted_by = UserReadSerializer(read_only=True)
+
+
+class AuditUserMixin:
+    """
+    Asignar el usuario que crea, actualiza o elimina un registro en las tablas que tienen los campos de auditoria.
+    """
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save(updated_by=self.request.user)
+
+    def perform_destroy(self, instance):
+        instance.deleted_by = self.request.user
+        instance.deleted_at = timezone.now()
+        instance.save(update_fields=["deleted_by", "deleted_at", "updated_at"])
--- backend/api/views.py
+++ backend/api/views.py
@@ -139,6 +139,7 @@ from .mixins.role_filter import RoleFilterMixin
 from .mixins.soft_delete_queryset import SoftDeleteQuerysetMixin
 from .mixins.operation_state import OperationStateMixin
 from .mixins.report_filter import ReportFilterMixin
+from .mixins.audit_fields import AuditUserMixin
 
 # Enums
 from nexus_inventory_backend.db.enums import OperationState, InvoiceState
@@ -159,6 +160,7 @@ class UserRoleViewSet(
     RoleFilterMixin,
     StateMixin,
     NoPutMixin,
+    AuditUserMixin,
     SoftDeleteQuerysetMixin,
     viewsets.ModelViewSet,
 ):
@@ -182,6 +184,7 @@ class UserViewSet(
     StrictFilterMixin,
     RoleFilterMixin,
     StateMixin,
+    AuditUserMixin,
     mixins.CreateModelMixin,
     mixins.ListModelMixin,
     viewsets.GenericViewSet,
@@ -254,6 +257,7 @@ class CategoryViewSet(
     RoleFilterMixin,
     StateMixin,
     NoPutMixin,
+    AuditUserMixin,
     SoftDeleteQuerysetMixin,
     viewsets.ModelViewSet,
 ):
@@ -278,6 +282,7 @@ class ProductViewSet(
     RoleFilterMixin,
     StateMixin,
     NoPutMixin,
+    AuditUserMixin,
     SoftDeleteQuerysetMixin,
     viewsets.ModelViewSet,
 ):
@@ -303,6 +308,7 @@ class InventoryViewSet(
     StrictFilterMixin,
     RoleFilterMixin,
     SoftDeleteQuerysetMixin,
+    AuditUserMixin,
     mixins.ListModelMixin,
     viewsets.GenericViewSet,
 ):
@@ -366,6 +372,7 @@ class CustomerViewSet(
     RoleFilterMixin,
     StateMixin,
     NoPutMixin,
+    AuditUserMixin,
     SoftDeleteQuerysetMixin,
     viewsets.ModelViewSet,
 ):
@@ -405,6 +412,7 @@ class PromotionViewSet(
     RoleFilterMixin,
     StateMixin,
     NoPutMixin,
+    AuditUserMixin,
     SoftDeleteQuerysetMixin,
     viewsets.ModelViewSet,
 ):
@@ -427,6 +435,7 @@ class CustomerPromotionViewSet(
     StrictFilterMixin,
     RoleFilterMixin,
     NoPutMixin,
+    AuditUserMixin,
     SoftDeleteQuerysetMixin,
     viewsets.ModelViewSet,
 ):
@@ -470,6 +479,7 @@ class SaleViewSet(
     RoleFilterMixin,
     OperationStateMixin,
     NoPutMixin,
+    AuditUserMixin,
     SoftDeleteQuerysetMixin,
     viewsets.ModelViewSet,
 ):
@@ -499,6 +509,9 @@ class SaleViewSet(
         if self.action == "cancel":
             return EmptySerializer
         return SaleReadSerializer
+
+    def perform_create(self, serializer):
+        pass
 
     @action(
         detail=True,
@@ -632,6 +645,7 @@ class SupplierViewSet(
     RoleFilterMixin,
     StateMixin,
     NoPutMixin,
+    AuditUserMixin,
     SoftDeleteQuerysetMixin,
     viewsets.ModelViewSet,
 ):
@@ -655,6 +669,7 @@ class PurchaseViewSet(
     RoleFilterMixin,
     OperationStateMixin,
     NoPutMixin,
+    AuditUserMixin,
     SoftDeleteQuerysetMixin,
     viewsets.ModelViewSet,
 ):
@@ -684,6 +699,9 @@ class PurchaseViewSet(
         if self.action == "cancel":
             return EmptySerializer
         return PurchaseReadSerializer
+
+    def perform_create(self, serializer):
+        pass
 
     @action(
         detail=True,

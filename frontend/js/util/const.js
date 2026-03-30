e3089ec0523dc67a333a65731d57db28bb0b59e3
jeironEC <jeironespinal0306@gmail.com>
2026-03-30 21:08:41 +0200

    NI-91: creación servicio de usuarios

--- frontend/js/util/const.js
+++ frontend/js/util/const.js
@@ -3,7 +3,11 @@ export const DEFAULT_TIMEOUT = 3000;
 // URLS ENDPOINTS
 export const URL_BASE = "http://127.0.0.1:8000"
 export const URL_AUTH_TOKEN = "/v1/auth/token/";
-export const URL_AUTH_REFRESH = "/v1/auth/refresh/";
+export const URL_AUTH_REFRESH = "/v1/auth/token/refresh/";
+export const URL_USER = "/v1/users/";
+export const URL_ACTIVE_USERS = "/v1/users/active/";
+export const URL_INACTIVE_USERS = "/v1/users/inactive/";
+export const URL_USER_PROFILE = "/v1/users/me/";
 
 // KEYS
 export const TOKEN_KEY = 'auth_token';

206f0a891d6eaab83b7c5abaf66d27c528f52fa3
jeironEC <jeironespinal0306@gmail.com>
2026-03-31 19:06:26 +0200

    NI-96: creación servicio de movimientos de inventario

--- frontend/js/util/const.js
+++ frontend/js/util/const.js
@@ -9,33 +9,36 @@ export const URL_BASE = "http://127.0.0.1:8000";
 
 // URLS TOKEN
 export const URL_AUTH_TOKEN = "/v1/auth/token/";
-export const URL_AUTH_REFRESH = "/v1/auth/token/refresh/";
+export const URL_AUTH_REFRESH = `${URL_AUTH_TOKEN}refresh/`;
 
 // URLS USERS
 export const URL_USERS = "/v1/users/";
-export const URL_ACTIVE_USERS = "/v1/users/active/";
-export const URL_INACTIVE_USERS = "/v1/users/inactive/";
-export const URL_USERS_PROFILE = "/v1/users/me/";
+export const URL_ACTIVE_USERS = `${URL_USERS}active/`;
+export const URL_INACTIVE_USERS = `${URL_USERS}inactive/`;
+export const URL_USERS_PROFILE = `${URL_USERS}me/`;
 
 // URLS ROLES
 export const URL_ROLES = "/v1/roles/";
-export const URL_ACTIVE_ROLES = "/v1/roles/active/";
-export const URL_INACTIVE_ROLES = "/v1/roles/inactive/";
+export const URL_ACTIVE_ROLES = `${URL_ROLES}active/`;
+export const URL_INACTIVE_ROLES = `${URL_ROLES}inactive/`;
 
 // URLS CATEGORIES
 export const URL_CATEGORIES = "/v1/categories/";
-export const URL_ACTIVE_CATEGORIES = "/v1/categories/active/";
-export const URL_INACTIVE_CATEGORIES = "/v1/categories/inactive/";
+export const URL_ACTIVE_CATEGORIES = `${URL_CATEGORIES}active/`;
+export const URL_INACTIVE_CATEGORIES = `${URL_CATEGORIES}inactive/`;
 
 // URLS PRODUCTS
 export const URL_PRODUCTS = "/v1/products/";
-export const URL_ACTIVE_PRODUCTS = "/v1/products/active/";
-export const URL_INACTIVE_PRODUCTS = "/v1/products/inactive/";
+export const URL_ACTIVE_PRODUCTS = `${URL_PRODUCTS}active/`;
+export const URL_INACTIVE_PRODUCTS = `${URL_PRODUCTS}inactive/`;
 
 // URLS INVENTORIES
 export const URL_INVENTORIES = "/v1/inventories/";
-export const URL_LOW_INVENTORIES = "/v1/inventories/low-stock/";
-export const URL_PRODUCT_INVENTORIES = "/v1/inventories/product/";
+export const URL_LOW_INVENTORIES = `${URL_INVENTORIES}low-stock/`;
+export const URL_PRODUCT_INVENTORIES = `${URL_INVENTORIES}product/`;
+
+// URLS INVENTORY MOVEMENTS
+export const URL_INVENTORY_MOVEMENTS = "/v1/inventory_movements/";
 
 // KEYS
 export const TOKEN_KEY = 'auth_token';

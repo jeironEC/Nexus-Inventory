53b07989dd88d6de70195df6edb4affbcd0307e5
Jeiron Espinal <jeironespinal0306@gmail.com>
2026-04-07 18:54:24 +0200

    NI-28: configuración de PostgreSQL como base de datos principal (#84)
    * NI-28: configuración de PostgreSQL como base de datos principal
    
    * NI-28: modificación del README
    
    * NI-28: corrigiendo workflow de tests
    
    * NI-28: corrigiendo workflow de tests

--- backend/nexus_inventory_backend/asgi.py
+++ backend/nexus_inventory_backend/asgi.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
+from configurations.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nexus_inventory_backend.settings")
+os.environ.setdefault("DJANGO_CONFIGURATION", "Local")
 
 application = get_asgi_application()

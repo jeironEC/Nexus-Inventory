452bf8b039d297a74c344a6d87c5a1ec9d810370
jeironEC <jeironespinal0306@gmail.com>
2026-04-09 01:26:44 +0200

    NI-118: configuración de SSL/TLS y HTTPS

--- backend/nexus_inventory_backend/settings.py
+++ backend/nexus_inventory_backend/settings.py
@@ -243,6 +243,20 @@ class Docker(Base):
     SECRET_KEY = env("SECRET_KEY", default="no_hack_me_please")
 
 
+class Production(Docker):
+    """Configuración para producción con SSL/TLS"""
+
+    DEBUG = False
+
+    SECURE_SSL_REDIRECT = True
+    SECURE_PROXY_SSL_HEADER = "HTTP_X_FORWARDED_PROTO,https"
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+
+
 class Test(Base):
     """Configuración para tests"""
 

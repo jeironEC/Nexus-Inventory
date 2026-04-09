e20d89419bda39a142948fe7410e7599bf48bbde
jeironEC <jeironespinal0306@gmail.com>
2026-04-09 17:38:48 +0200

    NI-117: implementación de monitoreo y observabilidad

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -9,6 +9,6 @@ python manage.py migrate --noinput
 echo "Arrancando Gunicorn"
 
 exec gunicorn \
-    --bind 127.0.0.1:8000 \
+    --bind 0.0.0.0:8000 \
     --workers $(nproc) \
     "nexus_inventory_backend.wsgi:application"

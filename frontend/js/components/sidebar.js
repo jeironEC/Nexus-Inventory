319b97fc0f656d1881360e687214b68ca2269b22
hunain17431 <mukhtarhunain7@gmail.com>
2026-04-28 21:56:46 +0200

    NI-131: creación vista de reporte de clientes

--- frontend/js/components/sidebar.js
+++ frontend/js/components/sidebar.js
@@ -139,7 +139,8 @@ class Sidebar {
                     { path: 'html/sales-report.html', page: 'reports/sales.html', icon: 'receipt_long', text: 'Reporte de Ventas' },
                     { path: 'html/purchases-report.html', page: 'reports/purchases.html', icon: 'shopping_bag', text: 'Reporte de Compras' },
                     { path: 'html/inventory-report.html', page: 'reports/inventory.html', icon: 'inventory_2', text: 'Reporte de Inventario' },
-                    { path: 'html/products-report.html', page: 'reports/products.html', icon: 'list_alt', text: 'Reporte de Productos' }
+                    { path: 'html/products-report.html', page: 'reports/products.html', icon: 'list_alt', text: 'Reporte de Productos' },
+                    { path: 'html/customers-report.html', page: 'reports/customers.html', icon: 'group', text: 'Reporte de Clientes' }
                 ]
             },
             {

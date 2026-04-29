39ba5158ebae6404c4187eb8427e7c2c5c6b8103
hunain17431 <mukhtarhunain7@gmail.com>
2026-04-29 12:50:16 +0200

    NI-136: refactor de tablas (proveedores, facturas e ID venta)

--- frontend/js/components/sidebar.js
+++ frontend/js/components/sidebar.js
@@ -136,13 +136,13 @@ class Sidebar {
             {
                 label: 'Reportes',
                 items: [
-                    { path: 'html/sales-report.html', page: 'reports/sales.html', icon: 'receipt_long', text: 'Reporte de Ventas' },
-                    { path: 'html/purchases-report.html', page: 'reports/purchases.html', icon: 'shopping_bag', text: 'Reporte de Compras' },
-                    { path: 'html/inventory-report.html', page: 'reports/inventory.html', icon: 'inventory_2', text: 'Reporte de Inventario' },
-                    { path: 'html/products-report.html', page: 'reports/products.html', icon: 'list_alt', text: 'Reporte de Productos' },
-                    { path: 'html/customers-report.html', page: 'reports/customers.html', icon: 'group', text: 'Reporte de Clientes' },
-                    { path: 'html/returns-report.html', page: 'reports/returns.html', icon: 'assignment_return', text: 'Reporte de Devoluciones' },
-                    { path: 'html/invoices-report.html',  page: 'reports/invoices.html',  icon: 'receipt',  text: 'Reporte de Facturación' }
+                    { path: 'html/sales_report.html', page: 'reports/sales.html', icon: 'receipt_long', text: 'Reporte de Ventas' },
+                    { path: 'html/purchases_report.html', page: 'reports/purchases.html', icon: 'shopping_bag', text: 'Reporte de Compras' },
+                    { path: 'html/inventory_report.html', page: 'reports/inventory.html', icon: 'inventory_2', text: 'Reporte de Inventario' },
+                    { path: 'html/products_report.html', page: 'reports/products.html', icon: 'list_alt', text: 'Reporte de Productos' },
+                    { path: 'html/customers_report.html', page: 'reports/customers.html', icon: 'group', text: 'Reporte de Clientes' },
+                    { path: 'html/returns_report.html', page: 'reports/returns.html', icon: 'assignment_return', text: 'Reporte de Devoluciones' },
+                    { path: 'html/invoices_report.html',  page: 'reports/invoices.html',  icon: 'receipt',  text: 'Reporte de Facturación' }
                 ]
             },
             {
